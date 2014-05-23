e2a2d76c8fae13f99f05fa14db26c980264bb458
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-23 09:27:21 -0500

    change value of submit->login

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -2,33 +2,30 @@
 <html>
 <head>
 
-<meta charset="utf-8">
-<title>"My First HTML Form"</title>
+	<meta charset="utf-8">
+	<title>"My First HTML Form"</title>
 
 </head>
 
 <body>
 
-
-
 <?php
 	var_dump($_GET);
 	var_dump($_POST);
-	?>
+?>
 
 
-
-<form method="POST">
+<form method="POST" action="/form.php">
 	<p>
 		<label for="name">Username</label>
-		<input id="name" name="username" tyoe="text" placeholder="Username here" required>
+		<input id="name" name="username" type="text" placeholder="Username" required>
 	</p>
 	<p>
 		<label for="pass">Passwords</label>
 		<input id="pass" name="password" type="password" placeholder="Password">
 	</p>
 	<p>
-		<input type="submit">
+		<button type="submit">Log In</button>
 	</p>
 </form>
 </body>
